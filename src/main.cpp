8146a313216947fcb1941792517e854c08dec64b
Chri6s <business.frimex@gmail.com>
2026-05-02 02:43:06 +0200

    feat: Add small test program for class relations

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
-#include <iostream>
+#include "gtest_lite.h"
 
+#include "basketball.h"
+#include "db_controller.h"
+#include "db_session.h"
+#include "db_status.h"
+#include "football.h"
+#include "handball.h"
+#include "menu.h"
+#include "sportclub.h"
+#include "team.h"
 
+#include <type_traits>
 
 int main() {
-    std::cout << "Hello CPP!" << std::endl;
-    return 1;
+    TEST(Relationships, Inheritance)
+        EXPECT_TRUE((std::is_base_of<Team, Basketball>::value));
+        EXPECT_TRUE((std::is_base_of<Team, Football>::value));
+        EXPECT_TRUE((std::is_base_of<Team, Handball>::value));
+    END
+
+    TEST(Interfaces, Signatures)
+        EXPECT_TRUE((std::is_same<decltype(&Team::setName), bool (Team::*)(const String&)>::value));
+        EXPECT_TRUE((std::is_same<decltype(&Team::setPlayerCount), bool (Team::*)(int)>::value));
+        EXPECT_TRUE((std::is_same<decltype(&DatabaseController::loadFromFile), DBStatus (DatabaseController::*)(const String&)>::value));
+        EXPECT_TRUE((std::is_same<decltype(&DatabaseSession::setDirty), bool (DatabaseSession::*)(bool)>::value));
+    END
+
+    TEST(Types, Existence)
+        EXPECT_TRUE((std::is_enum<DBStatus>::value));
+        EXPECT_TRUE((std::is_class<Menu>::value));
+        EXPECT_TRUE((std::is_class<SportClub>::value));
+    END
+
+    return 0;
 }
